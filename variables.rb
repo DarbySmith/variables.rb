4c4cd9aae5e76eb8d9ec150ac90e8bf406a51632
Darby Smith <newman.darby2@gmail.com>
2022-08-02 15:41:53 -0400

    Add variables

---
+++ variables.rb
@@ -0,0 +1,20 @@
+#Strings
+
+name = "darby"
+eye_color = "green"
+favorite_ice_cream = "cookie dough"
+
+# Integers
+
+age = 24
+height_inches = 64
+
+#Floats
+
+balance = 11.76
+grade = 90.2
+
+#Booleans
+
+college_graduate = true
+parent = false 
